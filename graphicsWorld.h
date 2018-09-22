920236f45d906616aceee303c66fafe6bc16467e
Shreya Patel <shreya984@hotmail.com>
2018-09-22 15:56:55 -0600

    created main.cpp and completed graphicsWorld

---
+++ graphicsWorld.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#ifndef GRAPHICSWORLD_H
+#define GRAPHICSWORLD_H
+
+class graphicsWorld {
+  public:
+    void run();
+  private:
+
+};
+#endif
---
+++ main.cpp
@@ -0,0 +1,14 @@
+
+//#include <assert.h>
+#include <iostream>
+#include "graphicsWorld.h"
+
+using namespace std;
+
+
+void main()
+{
+  graphicsWorld g;
+  g.run();
+  return;
+}
